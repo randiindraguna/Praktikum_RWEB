026abdb1ed301dcc22aa87d8b22e9414ece71ca9
randiindraguna <randiindraguna2203@gmail.com>
2020-06-27 23:36:30 -0700

    Project Rweb

---
+++ application/logs/log-2020-06-20.php
@@ -0,0 +1,87 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-06-20 09:54:41 --> Config Class Initialized
+INFO - 2020-06-20 09:54:41 --> Hooks Class Initialized
+DEBUG - 2020-06-20 09:54:41 --> UTF-8 Support Enabled
+INFO - 2020-06-20 09:54:41 --> Utf8 Class Initialized
+INFO - 2020-06-20 09:54:41 --> URI Class Initialized
+INFO - 2020-06-20 09:54:41 --> Router Class Initialized
+INFO - 2020-06-20 09:54:41 --> Output Class Initialized
+INFO - 2020-06-20 09:54:41 --> Security Class Initialized
+DEBUG - 2020-06-20 09:54:41 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-06-20 09:54:41 --> Input Class Initialized
+INFO - 2020-06-20 09:54:41 --> Language Class Initialized
+INFO - 2020-06-20 09:54:41 --> Loader Class Initialized
+INFO - 2020-06-20 09:54:41 --> Helper loaded: url_helper
+INFO - 2020-06-20 09:54:41 --> Helper loaded: captcha_helper
+INFO - 2020-06-20 09:54:41 --> Helper loaded: form_helper
+INFO - 2020-06-20 09:54:41 --> Database Driver Class Initialized
+INFO - 2020-06-20 09:54:41 --> Form Validation Class Initialized
+INFO - 2020-06-20 09:54:41 --> Session: Class initialized using 'files' driver.
+INFO - 2020-06-20 09:54:41 --> Controller Class Initialized
+INFO - 2020-06-20 09:54:41 --> Model Class Initialized
+INFO - 2020-06-20 09:54:41 --> File loaded: C:\xampp2\htdocs\sisfo_sma\application\views\login.php
+INFO - 2020-06-20 09:54:41 --> Final output sent to browser
+DEBUG - 2020-06-20 09:54:41 --> Total execution time: 0.1326
+INFO - 2020-06-20 09:54:47 --> Config Class Initialized
+INFO - 2020-06-20 09:54:47 --> Hooks Class Initialized
+DEBUG - 2020-06-20 09:54:47 --> UTF-8 Support Enabled
+INFO - 2020-06-20 09:54:47 --> Utf8 Class Initialized
+INFO - 2020-06-20 09:54:47 --> URI Class Initialized
+INFO - 2020-06-20 09:54:47 --> Router Class Initialized
+INFO - 2020-06-20 09:54:47 --> Output Class Initialized
+INFO - 2020-06-20 09:54:47 --> Security Class Initialized
+DEBUG - 2020-06-20 09:54:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-06-20 09:54:47 --> Input Class Initialized
+INFO - 2020-06-20 09:54:47 --> Language Class Initialized
+INFO - 2020-06-20 09:54:47 --> Loader Class Initialized
+INFO - 2020-06-20 09:54:47 --> Helper loaded: url_helper
+INFO - 2020-06-20 09:54:47 --> Helper loaded: captcha_helper
+INFO - 2020-06-20 09:54:47 --> Helper loaded: form_helper
+INFO - 2020-06-20 09:54:47 --> Database Driver Class Initialized
+INFO - 2020-06-20 09:54:47 --> Form Validation Class Initialized
+INFO - 2020-06-20 09:54:47 --> Session: Class initialized using 'files' driver.
+INFO - 2020-06-20 09:54:47 --> Controller Class Initialized
+INFO - 2020-06-20 09:54:47 --> Model Class Initialized
+INFO - 2020-06-20 09:54:47 --> Config Class Initialized
+INFO - 2020-06-20 09:54:47 --> Hooks Class Initialized
+DEBUG - 2020-06-20 09:54:47 --> UTF-8 Support Enabled
+INFO - 2020-06-20 09:54:47 --> Utf8 Class Initialized
+INFO - 2020-06-20 09:54:47 --> URI Class Initialized
+INFO - 2020-06-20 09:54:47 --> Router Class Initialized
+INFO - 2020-06-20 09:54:47 --> Output Class Initialized
+INFO - 2020-06-20 09:54:47 --> Security Class Initialized
+DEBUG - 2020-06-20 09:54:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-06-20 09:54:47 --> Input Class Initialized
+INFO - 2020-06-20 09:54:47 --> Language Class Initialized
+INFO - 2020-06-20 09:54:47 --> Loader Class Initialized
+INFO - 2020-06-20 09:54:47 --> Helper loaded: url_helper
+INFO - 2020-06-20 09:54:47 --> Helper loaded: captcha_helper
+INFO - 2020-06-20 09:54:47 --> Helper loaded: form_helper
+INFO - 2020-06-20 09:54:47 --> Database Driver Class Initialized
+INFO - 2020-06-20 09:54:47 --> Form Validation Class Initialized
+INFO - 2020-06-20 09:54:47 --> Session: Class initialized using 'files' driver.
+INFO - 2020-06-20 09:54:47 --> Controller Class Initialized
+INFO - 2020-06-20 09:54:47 --> Config Class Initialized
+INFO - 2020-06-20 09:54:47 --> Hooks Class Initialized
+DEBUG - 2020-06-20 09:54:47 --> UTF-8 Support Enabled
+INFO - 2020-06-20 09:54:47 --> Utf8 Class Initialized
+INFO - 2020-06-20 09:54:47 --> URI Class Initialized
+INFO - 2020-06-20 09:54:47 --> Router Class Initialized
+INFO - 2020-06-20 09:54:47 --> Output Class Initialized
+INFO - 2020-06-20 09:54:47 --> Security Class Initialized
+DEBUG - 2020-06-20 09:54:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-06-20 09:54:47 --> Input Class Initialized
+INFO - 2020-06-20 09:54:47 --> Language Class Initialized
+INFO - 2020-06-20 09:54:47 --> Loader Class Initialized
+INFO - 2020-06-20 09:54:47 --> Helper loaded: url_helper
+INFO - 2020-06-20 09:54:47 --> Helper loaded: captcha_helper
+INFO - 2020-06-20 09:54:47 --> Helper loaded: form_helper
+INFO - 2020-06-20 09:54:47 --> Database Driver Class Initialized
+INFO - 2020-06-20 09:54:47 --> Form Validation Class Initialized
+INFO - 2020-06-20 09:54:47 --> Session: Class initialized using 'files' driver.
+INFO - 2020-06-20 09:54:47 --> Controller Class Initialized
+INFO - 2020-06-20 09:54:47 --> Model Class Initialized
+INFO - 2020-06-20 09:54:47 --> File loaded: C:\xampp2\htdocs\sisfo_sma\application\views\login.php
+INFO - 2020-06-20 09:54:47 --> Final output sent to browser
+DEBUG - 2020-06-20 09:54:47 --> Total execution time: 0.0699
